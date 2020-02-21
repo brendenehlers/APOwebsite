fa7e70c7409c721697611f189d8451714e26821d
Brenden Ehlers <brenden@theehlers.org>
2020-02-21 16:32:54 -0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
 import { FullCalendarModule } from '@fullcalendar/angular';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { RouterModule } from '@angular/router';
 import appRoutes from './routesConfig';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { EventsComponent } from './events/events.component';
-import { CalendarComponent } from './calendar/calendar.component';
-
-import { CalendarModule, DateAdapter } from 'angular-calendar';
-import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     AboutComponent,
-    EventsComponent,
-    CalendarComponent
+    EventsComponent
   ],
   imports: [
     BrowserModule,
     NgbModule,
     BrowserAnimationsModule,
     RouterModule.forRoot(appRoutes),
-    CalendarModule.forRoot({ 
-      provide: DateAdapter, 
-      useFactory: adapterFactory 
-    }),
     FormsModule,
     FullCalendarModule
   ],
--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -1,18 +1,32 @@
-import { Component, ViewChild, DebugElement } from '@angular/core';
+import { Component, ViewChild, Input, OnInit } from '@angular/core';
 
 import { FullCalendarComponent } from '@fullcalendar/angular';
 // import { EventInput } from '@fullcalendar/core';
-
 import dayGridPlugin from '@fullcalendar/daygrid';
+import listPlugin from '@fullcalendar/list';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+
+import * as data from './events.json';
 
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
   styleUrls: ['./events.component.css']
 })
-export class EventsComponent {
+export class EventsComponent implements OnInit {
 
-  constructor() { }
+  events = (data as any).default;
+
+  //modal values
+  modalVisible: boolean = false;
+  modalTitle: string = null;
+  modalStart: Date = null;
+  modalEnd: Date = null;
+  modalDesc: string = null;
+
+  closeResult: string;
+
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit(): void {
   }
@@ -20,22 +34,36 @@ export class EventsComponent {
   @ViewChild('calendar') calendarComponent: FullCalendarComponent;
 
   calendarVisible = true;
-  calendarPlugins = [dayGridPlugin];
+  calendarPlugins = [dayGridPlugin, listPlugin];
 
-  events = [
-    {title: 'event now', start: '2020-01-01', end: '2020-01-02'}
-  ]
+  open(content, eventCall) {
 
-  // calendarEvents: EventInput[] = [
-  //   { title: 'Event Now', start: new Date() }
-  // ];
-
-  toggleVisible() {
-    // this.calendarVisible = !this.calendarVisible;
-    this.calendarVisible = !this.calendarVisible;
     debugger;
+    this.modalTitle = eventCall.event.title;
+    this.modalStart = eventCall.event.start;
+    this.modalEnd = eventCall.event.end;
+    this.modalDesc = eventCall.event.borderColor;
+
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title', size: 'lg'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+
+    return false;
   }
 
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
+
+  eventClickModalClose(){
+    this.modalVisible = false;
+  }
 }
-
-
